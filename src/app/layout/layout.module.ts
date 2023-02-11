3f4e4622e5ed00f495ab2f12231444fc2411e8ba
Matheus Alencar <matheusalencar6942@gmail.com>
2023-02-11 18:35:38 -0300

    segmentando header e sidebar

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 import { LayoutComponent } from './layout.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [LayoutComponent],
+  declarations: [LayoutComponent, SidebarComponent, HeaderComponent],
   imports: [CommonModule],
   exports: [LayoutComponent],
 })
