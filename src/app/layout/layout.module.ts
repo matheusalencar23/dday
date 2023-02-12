0f29656df4bd4c8df03a905a9d72008a6f4a278f
Matheus Alencar <matheusalencar6942@gmail.com>
2023-02-12 15:35:23 -0300

    adicionando icones

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,5 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { NgIconsModule } from '@ng-icons/core';
+import {
+  heroHomeSolid,
+  heroShoppingBagSolid,
+  heroUserGroupSolid,
+  heroUserSolid,
+} from '@ng-icons/heroicons/solid';
+import { tablerPigMoney } from '@ng-icons/tabler-icons';
 import { LogoModule } from '../components/logo/logo.module';
 import { HeaderComponent } from './header/header.component';
 import { LayoutComponent } from './layout.component';
@@ -7,7 +15,17 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [LayoutComponent, SidebarComponent, HeaderComponent],
-  imports: [CommonModule, LogoModule],
+  imports: [
+    CommonModule,
+    LogoModule,
+    NgIconsModule.withIcons({
+      heroHomeSolid,
+      heroShoppingBagSolid,
+      heroUserSolid,
+      heroUserGroupSolid,
+      tablerPigMoney,
+    }),
+  ],
   exports: [LayoutComponent],
 })
 export class LayoutModule {}
--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { SidebarItem } from '../models/sidebar-item';
 
 @Component({
   selector: 'dd-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  sidebarItems: SidebarItem[] = [
+    {
+      icon: 'heroHomeSolid',
+      label: 'Dashboard',
+    },
+    {
+      icon: 'heroShoppingBagSolid',
+      label: 'Payment',
+    },
+    {
+      icon: 'heroUserSolid',
+      label: 'Employees',
+    },
+    {
+      icon: 'heroUserGroupSolid',
+      label: 'Departments',
+    },
+    {
+      icon: 'tablerPigMoney',
+      label: 'Liquidity',
+    },
+  ];
+}
