1cff9e5b42712883ad2ab928ae48c76ccd3962e8
Matheus Alencar <matheusalencar6942@gmail.com>
2023-02-12 15:11:20 -0300

    component logo

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { LogoModule } from '../components/logo/logo.module';
 import { HeaderComponent } from './header/header.component';
 import { LayoutComponent } from './layout.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [LayoutComponent, SidebarComponent, HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, LogoModule],
   exports: [LayoutComponent],
 })
 export class LayoutModule {}
