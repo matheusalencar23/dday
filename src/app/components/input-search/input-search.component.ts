4c6bf41dce48691fa3829849e4b4ff26763d59b0
Matheus Alencar <matheusalencar6942@gmail.com>
2023-05-18 21:25:33 -0300

    convertendo input de busca

--- src/app/components/input-search/input-search.component.ts
+++ src/app/components/input-search/input-search.component.ts
@@ -1,5 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, Input, Renderer2 } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  Renderer2,
+  forwardRef,
+} from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormsModule,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { heroMagnifyingGlassSolid } from '@ng-icons/heroicons/solid';
 
@@ -8,10 +19,17 @@ import { heroMagnifyingGlassSolid } from '@ng-icons/heroicons/solid';
   templateUrl: './input-search.component.html',
   styleUrls: ['./input-search.component.scss'],
   standalone: true,
-  imports: [CommonModule, NgIconComponent],
-  providers: [provideIcons({ heroMagnifyingGlassSolid })],
+  imports: [CommonModule, NgIconComponent, FormsModule],
+  providers: [
+    provideIcons({ heroMagnifyingGlassSolid }),
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputSearchComponent),
+      multi: true,
+    },
+  ],
 })
-export class InputSearchComponent {
+export class InputSearchComponent implements ControlValueAccessor {
   protected _hasBorder: boolean = false;
 
   @Input() placeholder: string = '';
@@ -20,5 +38,26 @@ export class InputSearchComponent {
     if (value) this.renderer.addClass(this.elRef.nativeElement, 'bordered');
   }
 
+  value: string = '';
+  disabled: boolean = false;
+  onChange: any = () => {};
+  onTouch: any = () => {};
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
 }
--- src/app/pages/internal/payment/payment.component.ts
+++ src/app/pages/internal/payment/payment.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { InputSearchComponent } from 'src/app/components/input-search/input-search.component';
 import { PageTitleComponent } from 'src/app/components/page-title/page-title.component';
 
@@ -8,6 +9,13 @@ import { PageTitleComponent } from 'src/app/components/page-title/page-title.com
   templateUrl: './payment.component.html',
   styleUrls: ['./payment.component.scss'],
   standalone: true,
-  imports: [CommonModule, PageTitleComponent, InputSearchComponent],
+  imports: [
+    CommonModule,
+    PageTitleComponent,
+    InputSearchComponent,
+    FormsModule,
+  ],
 })
-export class PaymentComponent {}
+export class PaymentComponent {
+  searchTerm: string = '';
+}
