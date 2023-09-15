3a46c2aec0c76173e5ceef71f2dfe1af597e45fa
Matheus Alencar <matheusalencar6942@gmail.com>
2023-09-15 10:10:35 -0300

    removendo arquivos cagados

--- src/app/pages/internal/payment/payment.component.ts
+++ src/app/pages/internal/payment/payment.component.ts
@@ -1,10 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { DefaultFilterComponent } from 'src/app/components/default-filter/default-filter.component';
 import { PageTitleComponent } from 'src/app/components/page-title/page-title.component';
-import { TableComponent } from 'src/app/components/table/table.component';
-import { IdChipDirective } from 'src/app/directives/id-chip.directive';
 import { PAYMENTS } from 'src/app/mocks/payment';
 import { Filter } from 'src/app/models/filter';
 import { Payment } from 'src/app/models/payment';
@@ -14,14 +11,7 @@ import { Payment } from 'src/app/models/payment';
   templateUrl: './payment.component.html',
   styleUrls: ['./payment.component.scss'],
   standalone: true,
-  imports: [
-    CommonModule,
-    PageTitleComponent,
-    FormsModule,
-    DefaultFilterComponent,
-    TableComponent,
-    IdChipDirective,
-  ],
+  imports: [CommonModule, PageTitleComponent, FormsModule],
 })
 export class PaymentComponent implements OnInit {
   payments: Payment[] = [];
