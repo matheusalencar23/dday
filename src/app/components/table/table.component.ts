bcbb5aff61164d7d9ff843b779cad51c8e5f3805
Matheus Alencar <matheusalencar6942@gmail.com>
2023-10-26 06:35:03 -0300

    melhorando implementação de cores do id das tabelas

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -13,7 +13,27 @@ import { tablerCaretDown, tablerCaretUp } from '@ng-icons/tabler-icons';
   providers: [provideIcons({ tablerCaretUp, tablerCaretDown })],
 })
 export class TableComponent {
+  colors = [
+    { bgColor: '#bdd9fe', color: '#217efd' },
+    { bgColor: '#ffb5364a', color: '#ffb536' },
+    { bgColor: '#f4cefa', color: '#db5aee' },
+    { bgColor: '#fbd1c9', color: '#f3654a' },
+    { bgColor: '#c5d1df', color: '#384455' },
+    { bgColor: '#dccefb', color: '#895bf1' },
+    { bgColor: '#27ae604a', color: '#27ae60' },
+    { bgColor: '#56ccf24a', color: '#56ccf2' },
+  ];
+
   @Input() tableConfig: DataTableType = { columns: [] };
 
   @Input() tableContent: any[] = [];
+
+  getColors(): { [klass: string]: any } | null | undefined {
+    const randomIndex = Math.floor(Math.random() * this.colors.length);
+
+    return {
+      backgroundColor: this.colors[randomIndex].bgColor,
+      color: this.colors[randomIndex].color,
+    };
+  }
 }
