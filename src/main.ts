4ecd6cfdead5c81eaf70ee4e4292f8f78fd21aff
Matheus Alencar <matheusalencar6942@gmail.com>
2024-12-19 22:54:46 -0300

    adicionando biblioteca de internacionalização e iniciando primeiros cases

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,29 @@ import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { AppRoutes } from './app/routes';
+import { importProvidersFrom } from '@angular/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideAnimations(), provideRouter(AppRoutes)],
+  providers: [
+    provideAnimations(),
+    provideRouter(AppRoutes),
+    provideHttpClient(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
+        defaultLanguage: 'en',
+      })
+    ),
+  ],
 }).catch((err) => console.error(err));
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
