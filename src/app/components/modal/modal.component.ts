d93ecc9ba03358ea6b0050a1d5942991c75370c5
Matheus Alencar <matheusalencar6942@gmail.com>
2025-02-18 08:20:55 -0300

    ajustando animação do componente de modal

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -8,6 +8,7 @@ import {
 import { CommonModule } from '@angular/common';
 import { ModalOptions } from 'src/app/models/modal-options';
 import { ModalService } from 'src/app/services/modal.service';
+import { fromEvent, Observable, zip } from 'rxjs';
 
 @Component({
   selector: 'dd-modal',
@@ -20,6 +21,15 @@ export class ModalComponent implements AfterViewInit {
   @ViewChild('modal') modal!: ElementRef<HTMLDivElement>;
   @ViewChild('overlay') overlay!: ElementRef<HTMLDivElement>;
   options!: ModalOptions | undefined;
+
+  modalLeaveAnimation!: string;
+  overlayLeaveAnimation!: string;
+
+  modalAnimationEnd!: Observable<Event>;
+  overlayAnimationEnd!: Observable<Event>;
+
+  modalLeaveTiming!: number;
+  overlayLeaveTiming!: number;
 
   constructor(
     private modalService: ModalService,
@@ -55,11 +65,53 @@ export class ModalComponent implements AfterViewInit {
     this.modal.nativeElement.style.height = this.options?.height || 'auto';
     this.modal.nativeElement.style.maxHeight =
       this.options?.maxHeight || 'auto';
+
+    this.modalLeaveAnimation = 'fade-out 0.3s forwards';
+    this.overlayLeaveAnimation = 'fade-out 0.3s forwards';
+
+    this.modalAnimationEnd = this.animationendEvent(this.modal.nativeElement);
+    this.overlayAnimationEnd = this.animationendEvent(
+      this.overlay.nativeElement
+    );
+
+    this.modalLeaveTiming = this.getAnimationTime(this.modalLeaveAnimation);
+    this.overlayLeaveTiming = this.getAnimationTime(this.overlayLeaveAnimation);
+  }
+
+  animationendEvent(element: HTMLDivElement) {
+    return fromEvent(element, 'animationend');
   }
 
   close() {
-    this.modalService.options = undefined;
-    this.element.nativeElement.remove();
-    return;
+    this.modal.nativeElement.style.animation = this.modalLeaveAnimation;
+    this.overlay.nativeElement.style.animation = this.overlayLeaveAnimation;
+
+    if (this.modalLeaveTiming > this.overlayLeaveTiming) {
+      this.modalAnimationEnd.subscribe(() => {
+        this.element.nativeElement.remove();
+      });
+    } else if (this.modalLeaveTiming < this.overlayLeaveTiming) {
+      this.overlayAnimationEnd.subscribe(() => {
+        this.element.nativeElement.remove();
+      });
+    } else {
+      zip(this.modalAnimationEnd, this.overlayAnimationEnd).subscribe(() => {
+        this.element.nativeElement.remove();
+      });
+    }
+  }
+
+  getAnimationTime(animation: string) {
+    let animationTime = 0;
+    const splittedAnimation = animation.split(' ');
+    for (const expression of splittedAnimation) {
+      const currentValue = +expression.replace(/s$/, '');
+      if (!isNaN(currentValue)) {
+        animationTime = currentValue;
+        break;
+      }
+    }
+
+    return animationTime;
   }
 }
